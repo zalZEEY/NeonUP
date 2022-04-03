5174bda316bc3bd4d72a017fb735d6675ad3b3ee
Z E E Y <88220322+zalZEEY@users.noreply.github.com>
2022-04-04 03:25:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const PORT = process.env.PORT || 5000
 const { Client, Collection } = require("discord.js");
 const UrlsConfig = require("./database/models/UrlsConfig");
 const fetchProjects = require("./fetchProjects");
