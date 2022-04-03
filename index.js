d7ca57deabb8c2d99e1ee6f2f430aa18215fb329
Z E E Y <88220322+zalZEEY@users.noreply.github.com>
2022-04-03 20:06:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 require("dotenv").config();
 const { Client, Collection } = require("discord.js");
 const UrlsConfig = require("./database/models/UrlsConfig");
