3b533d4cd0683dd3fb84f55bbd939e7402f853e5
Z E E Y <88220322+zalZEEY@users.noreply.github.com>
2022-04-18 17:24:42 +0700

    Create ping.js

---
+++ commands/uptime/ping.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "ping",
+  category: "info",
+  description: "Get bot ping :/",
+  usage: "ping",
+  run: (client, message) => {
+    message.channel.send(`**:ping_pong:Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms.:ping_pong:**`);
+  }
+  
+}
