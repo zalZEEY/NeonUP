8dbd10045fb45a997239197a37d88219d15b3af0
Z E E Y <88220322+zalZEEY@users.noreply.github.com>
2022-04-18 18:41:17 +0700

    Update uptime.js

--- commands/uptime/uptime.js
+++ commands/uptime/uptime.js
@@ -1,35 +1,19 @@
 const UrlsConfig = require("./../../database/models/UrlsConfig");
 const { MessageEmbed } = require("discord.js");
 const { default_prefix } = require("./../../config.json");
-
+const Discord = require('discord.js');
+const moment = require("moment");
+const bot = new Discord.Client();
+require("moment-duration-format");
 module.exports = {
-name: 'uptime',
-               description: 'Gives you uptime of the bot',
-               async execute(client, message, args) {
-                let days = 0
-            let week = 0
-            let uptime = '';
-            let totalSeconds = (client.uptime / 1000)
-            let hours = Math.floor(totalSeconds / 3600)
-            totalSeconds %= 3600
-            let minutes = Math.floor(totalSeconds / 60)
-            let seconds = Math.floor(totalSeconds % 60)
-            if (hours > 24) {
-            days = days + 1
-            hours = 0
-                    }
-            if (week - 0) {
-            uptime += `${week} week, `
-                    }
-            if (minutes > 60) {
-            minutes = 0;
-                    }
-            uptime += `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`
-            
-            await message.channel.send(`my uptime is ${uptime}`)
-            
-            
-              }
-            }``` 
-            
-        perfection
+    name: 'uptime',
+    description: "Bot Stats",
+    execute(message, args){ 
+        const duration = moment.duration(bot.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+        const statEmbed = new Discord.RichEmbed()
+            .setTitle("**  = STATISTICS =**")
+            .addField("**Mem Usage  ::**", `**${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB**`)
+            .addField("**Uptime**", `**${duration}**`);
+        message.channel.send(statEmbed);
+    }
+}
