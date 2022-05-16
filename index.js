a977df9fa7b5b08a7a9156f43883f5213c503345
Z E E Y <88220322+zalZEEY@users.noreply.github.com>
2022-05-16 23:28:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 require('http').createServer((req, res) => res.end('Bot is alive!')).listen(process.env.PORT, '0.0.0.0')
-const client = new Discord.Client({ ws: { properties: { $browser: "Discord iOS" }} });
-const { Client, Collection } = require("discord.js");
+const { Client, Collection } = require("Discord iOS");
 const UrlsConfig = require("./database/models/UrlsConfig");
 const fetchProjects = require("./fetchProjects");
 const { timeout, disable_fetching } = require("./config.json");
