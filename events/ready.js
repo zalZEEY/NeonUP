f1b9d7282d19a63fac4fabfa07d19b7fba8b382c
Z E E Y <88220322+zalZEEY@users.noreply.github.com>
2022-08-13 17:34:21 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,6 @@ module.exports.run = async (client) => {
   console.log(`${client.user.tag} has logged in.`);
   client.user.setActivity(`${client.guilds.cache.size} Servers and ${client.projectsSize} Project(s)`, {
     type: "WATCHING",
+    url: "https://www.twitch.tv/leo_zeey" 
   });
 };
